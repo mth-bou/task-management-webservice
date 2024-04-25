8b0f8b879de3a4e64637463ff8a3fb051b2c913f
Mathieu <mathieu.boucher@epsi.fr>
2024-04-25 16:59:19 +0200

    Add configs and logger

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,2 +1,36 @@
 const winston = require('winston');
-const config = require('./config');
+const path = require('path');
+
+const logger = winston.createLogger({
+    defaultMeta: { service: 'task-management' },
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+    transports: [],
+});
+
+module.exports = logger;
+
+const init = ({ env, logLevel: level}) => {
+    logger.add(
+      new winston.transports.Console({
+          level,
+          silent: env === 'test',
+      }),
+    );
+
+    if (env !== 'development') {
+        logger.add(
+          new winston.transports.File({
+              level,
+              filename: path.join(__dirname, '../../logs/app.log'),
+          }),
+        );
+    }
+}
+
+const destroy = () => {
+    logger.clear();
+    logger.close();
+}
+
+module.exports.init = init;
+module.exports.destroy = destroy;
