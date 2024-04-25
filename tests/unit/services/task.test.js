13e0c4544f575dec28c4043ba114a9428f7adccc
Mathieu <mathieu.boucher@epsi.fr>
2024-04-25 19:57:23 +0200

    add unit tests for taskService

---
+++ tests/unit/services/task.test.js
@@ -0,0 +1,37 @@
+const { faker } = require('@faker-js/faker');
+const { Task } = require('../../../src/models');
+const { taskService} = require('../../../src/services');
+
+afterEach(() => {
+    jest.restoreAllMocks();
+});
+
+describe('TaskService', () => {
+
+    describe('getTaskById', () => {
+        it('should return a task', async () => {
+            const objId = faker.string.uuid();
+            const name = faker.lorem.words(1);
+            const description = faker.lorem.sentence();
+            const task = { id: objId, name, description };
+            jest.spyOn(Task, 'findById').mockImplementation((id) => (objId === id ? task : undefined));
+            const result = await taskService.getTaskById(objId);
+            expect(result.id).toEqual(objId);
+            expect(result.name).toEqual(name);
+            expect(result.description).toEqual(description);
+            expect(Task.findById).toBeCalledWith(objId);
+        });
+    });
+
+    describe('createTask', () => {
+        it('should create a task', async () => {
+            const name = faker.string.uuid();
+            const description = faker.lorem.sentence();
+            jest.spyOn(Task, 'create').mockResolvedValue({ name, description });
+            const result = await taskService.createTask(name, description);
+            expect(result.name).toEqual(name);
+            expect(result.description).toEqual(description);
+            expect(Task.create).toBeCalledWith({ name, description });
+        });
+    });
+});
