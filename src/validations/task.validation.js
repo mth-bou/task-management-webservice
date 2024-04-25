85e9ce2fcdbb8632cf32b4743499ede9f64aacf4
Mathieu <mathieu.boucher@epsi.fr>
2024-04-25 16:21:38 +0200

    Add task validation

--- src/validations/task.validation.js
+++ src/validations/task.validation.js
@@ -1,8 +1,33 @@
-const getTaskById = () => {
+const Joi = require('joi');
 
+const objectId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+const getTaskById = {
+    params: Joi.object().keys({
+        id: objectId.required(),
+    }),
+};
+
+const createTask = {
+    body: Joi.object().keys({
+        name: Joi.string().required(),
+        description: Joi.string().optional(),
+    }),
+}
+
+const updateTaskById = {
+    params: Joi.object().keys({
+        id: objectId.required()
+    }),
+    body: Joi.object().keys({
+        name: Joi.string().optional(),
+        description: Joi.string().optional(),
+        status: Joi.string().valid('new', 'active', 'completed', 'cancelled').optional().default('new'),
+    }),
 }
 
 module.exports = {
     getTaskById,
-
-}
+    createTask,
+    updateTaskById,
+};
